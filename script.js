0c1f874ece234d6927d315327439a6f79a3e2909
Nutan  Mandal <nutanmandal4888@gmail.com>
2025-01-30 01:29:59 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+<script>
+        document.querySelector('input[type="button"]').addEventListener('click', function() {
+            let select = document.querySelector("#colorSelect");
+            let selectedIndex = select.selectedIndex;
+
+            if (selectedIndex !== -1) {
+                select.remove(selectedIndex);
+            }
+        });
+    </script>
